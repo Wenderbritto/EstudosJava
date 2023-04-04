11a026e6e0f3b42f7e8999662f3695c403c2986b
wenderbritto <wenderbrito.dev@gmail.com>
2023-04-04 15:44:13 -0300

    Rename

---
+++ ClasseAtributosObjeto01/Conta.java
@@ -0,0 +1,10 @@
+package ClasseAtributosObjeto01;
+
+//Crie uma classe chamada "Conta"
+public class Conta {
+
+    //Crie os atributos público da classe Conta (saldo, numero, limite)
+    public float saldo;
+    public int numero;
+    public float limite;
+}
