45752a3619a1b2391366cdd90c77056d522fb397
wenderbritto <wenderbrito.dev@gmail.com>
2023-04-30 16:56:39 -0300

    Heranca

---
+++ Heranca/Main.java
@@ -0,0 +1,12 @@
+package Heranca;
+
+class Main {
+   
+    public static void main(String[] args) {
+
+        new Funcionario("",123,"");
+        
+        new Coordenador("", 123, "");
+    }
+    }
+
